2faa5beda9c6d73717f91cfa9e6f0bdb09c297c2
MiguelTucto <u201521691@upc.edu.pe>
2024-06-21 23:21:46 -0500

    feat: words has been translated

--- detection/services.py
+++ detection/services.py
@@ -7,9 +7,21 @@ from .model import YoloV8Model
 yolo_model = YoloV8Model('../model/yolov8l.pt')
 
 HUMAN_CLASS_IDS = [0]
+
+# Diccionario de traducción de labels
+LABEL_TRANSLATIONS = {
+    "carrot": "Zanahoria",
+    "apple": "Manzana",
+    "car": "Coche",
+    # Agrega aquí más traducciones según tus necesidades
+}
+
 def decode_image(image_data):
     image_data = base64.b64decode(image_data)
     return Image.open(BytesIO(image_data))
+
+def translate_label(label):
+    return LABEL_TRANSLATIONS.get(label, label)
 
 def detect_objects(image_data):
     image = decode_image(image_data)
@@ -25,11 +37,15 @@ def detect_objects(image_data):
             cls = box.cls[0].item()
             label = yolo_model.model.names[int(cls)]
             if int(cls) not in HUMAN_CLASS_IDS:  # Filtrar detecciones de humanos
+                translated_label = translate_label(label)
                 width = x2 - x1
                 height = y2 - y1
+                centerX = (x1 + x2) / 2
+                centerY = (y1 + y2) / 2
+                radius = max(width, height) / 2
 
                 detected_products.append({
-                    'name': label,
+                    'name': translated_label,
                     'confidence': float(conf),
                     'coordinates': {
                         'x1': x1,
@@ -43,6 +59,6 @@ def detect_objects(image_data):
                         'radius': max(width, height) / 2
                     }
                 })
-                print(f"Detected: {label} (Confidence: {conf}) at coordinates: {x1}, {y1}, {x2}, {y2}, with center at {detected_products[-1]['coordinates']['centerX']}, {detected_products[-1]['coordinates']['centerY']} and radius {detected_products[-1]['coordinates']['radius']}")
+                print(f"Detected: {translated_label} (Confidence: {conf}) at coordinates: {x1}, {y1}, {x2}, {y2}, with center at {centerX}, {centerY} and radius {radius}")
 
     return detected_products
